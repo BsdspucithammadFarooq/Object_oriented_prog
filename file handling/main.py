ae11454b5b0f0a12b95e06f43cd189276977cd86
bsdsf22a026@pucit.edu.pk <136726081+BsdspucithammadFarooq@users.noreply.github.com>
2023-12-10 14:10:21 +0500

    Add files via upload

---
+++ file handling/main.py
@@ -0,0 +1,7 @@
+with open (file="data.txt",mode="a") as file:
+    # file.seek(6)
+    # file.tell()
+    # print(file.readline())
+    # file.truncate(2)
+    # file.flush() it will flush the buffer ,and done automatically in python when we close a file
+    
